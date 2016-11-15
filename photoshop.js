6e1fff04590ab8ee7ecf49b87f10fd10034ab6a4
singhhrpreet <thesinghharpreet@gmail.com>
2016-11-15 20:59:17 +0530

    Intial Boilerplate

---
+++ photoshop.js
@@ -0,0 +1,7 @@
+/**
+* PhotoShop Web Javascript Library
+* Version: 0.0.1
+* Author: singhhrpreet
+* Visit: singhhrpreet.wordpress.com
+* Mail: singhhrpreet@outlook.com, thesinghharpreet@gmail.com
+*/
